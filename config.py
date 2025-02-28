1562e394b6f4391074282186aba97599790e4847
tedoaba <tedoaba@gmail.com>
2025-03-01 00:34:44 +0300

    Positional encoding and self-attention

--- config.py
+++ config.py
@@ -4,11 +4,12 @@ class Config:
     def __init__(self):
         self.batch_size = 32
         self.block_size = 8
-        self.max_iters = 3000
-        self.eval_interval = 300
-        self.learning_rate = 1e-2
+        self.max_iters = 5000
+        self.eval_interval = 500
+        self.learning_rate = 1e-3
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.eval_iters = 50
+        self.eval_iters = 200
+        self.n_embed = 32
         self.seed = 1337
         self.data_path = 'data.txt'
         self.split_ratio = 0.9
--- model.py
+++ model.py
@@ -1,16 +1,47 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from config import config
+
+class Head(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(config.n_embed, head_size, bias=False)
+        self.query = nn.Linear(config.n_embed, head_size, bias=False)
+        self.value = nn.Linear(config.n_embed, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(config.block_size, config.block_size)))
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+
+        # compute attention scores
+        wei = q @ k.transpose(-2, -1) * (C ** 0.5)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        v = self.value(x)
+        out = wei @ v
+        return out
 
 class BigramLanguageModel(nn.Module):
 
     def __init__(self, vocab_size):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, config.n_embed)
+        self.position_embedding_table = nn.Embedding(config.block_size, config.n_embed)
+        self.sa_head = Head(config.n_embed)
+        self.lm_head = nn.Linear(config.n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
 
-        logits = self.token_embedding_table(idx)
+        B, T = idx.shape
+        tok_embed = self.token_embedding_table(idx)
+        pos_embed = self.position_embedding_table(torch.arange(T, device=idx.device))
+        x = tok_embed + pos_embed
+        x = self.sa_head(x)
+        logits = self.lm_head(x)
 
         if targets is None:
             loss = None
@@ -25,7 +56,8 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
 
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
+            idx_cond = idx[: , -config.block_size:]
+            logits, loss = self(idx_cond)
             logits = logits[:, -1, :]
             probs = F.softmax(logits, dim=1)
             idx_next = torch.multinomial(probs, num_samples=1)
